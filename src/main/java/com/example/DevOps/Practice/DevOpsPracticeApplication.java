2cf67111e68eb39d86b71ef50a2e39e1b842c642
Ishmam Ruhan <ishmamruhan@gmail.com>
2022-09-17 16:10:59 +0600

    project init

---
+++ src/main/java/com/example/DevOps/Practice/DevOpsPracticeApplication.java
@@ -0,0 +1,29 @@
+package com.example.DevOps.Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevOpsPracticeApplication {
+
+	@GetMapping("/sayHi")
+	public String sayHi(){
+		return "Hey! Welcome dev-ops practice! I'm from system. Whats your name?";
+	}
+
+	@GetMapping("/name")
+	public String sayName(@PathVariable String name){
+		return "Wow.! Nice name "+name+"\n  Let's explore more!";
+	}
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DevOps/Practice/DevOpsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DevOps.Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
